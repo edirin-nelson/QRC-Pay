8087bffded427d6abb69fdc1c318f5656d247044
edirin nelson <nelsonedirin@gmail.com>
2023-08-06 18:08:01 +0100

    Initial commit

---
+++ src/main/java/com/isdservice/qrcpay/QrcPayApplication.java
@@ -0,0 +1,13 @@
+package com.isdservice.qrcpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isdservice/qrcpay/QrcPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isdservice.qrcpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
